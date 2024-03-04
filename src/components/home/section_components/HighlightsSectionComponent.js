3abd18f807b8d1ebb7496d2a7d652126110ba28b
Naveen s choudhary <naveenschoudhary.work@gmail.com>
2024-03-04 09:59:05 +0530

    Update date

--- src/components/home/section_components/HighlightsSectionComponent.js
+++ src/components/home/section_components/HighlightsSectionComponent.js
@@ -23,7 +23,7 @@ const HighlightsSectionComponent = () => {
           <h1 className="text-3xl">Diverse Venues</h1>
           <div className="w-40 mt-4 h-0.5 bg-[#E0B279]"></div>
           <p className="py-8 px-4 md:px-24  lg:px-72 text-lg">
-            Be it an intimate affair or a big, fat Indian wedding, we've got the
+            Be it an intimate affair or a big, fat Indian wedding, we have got the
             perfect space for you. Our resort offers a variety of venues, from a
             lavish banquet hall to lush green gardens, each crafted to host
             every aspect of your celebration.
